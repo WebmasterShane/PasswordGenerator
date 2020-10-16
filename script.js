94d83623360cbfbd8e2b68273c0042e19ef7e57c
WebmasterShane <shanelwebber@gmail.com>
2020-10-16 03:18:46 -0700

    my first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// Assignment Code
+//var generateBtn = document.querySelector("#generate");
+//var characterSet="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ，．：；！？＂＇｀＾～￣＿＆＠＃％＋－＊＝＜＞（）［］｛｝｟｠｜￤／＼￢＄"
+//var pwLength = prompt("How many characters would you like in your password?")
+//var lowerCaseChar = confirm("would you like lower case letters?")
+//var upperCaseChar = confirm("would you like upper case letters?")
+//var numberCaseChar = confirm("would you like numbers?")
+//var specialCaseChar = confirm("would you like special characters?")
+
+
+console.log("This is a test")
+
+function getLower(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+
+
+}
+function getUpper(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+
+
+}
+console.log(getLower());
+console.log(getUpper());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//generateBtn.addEventListener("click", function(){
+//var password
+//for (var i=0; i=pwLength; i++)
+
+
+//});
+
+//console.log(password)
